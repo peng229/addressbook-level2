a1ae7faa51cf45f0b874f7a236d7bf352a7549a2
peng229 <pc_tony229@hotmail.com>
2017-02-12 23:18:14 +0800

    add new test case for different case names. JUnit test fails.

--- src/seedu/addressbook/data/person/NameTest.java
+++ src/seedu/addressbook/data/person/NameTest.java
@@ -2,16 +2,32 @@ package seedu.addressbook.data.person;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 import seedu.addressbook.data.exception.IllegalValueException;
 
 public class NameTest {
+	Name first;
+	Name second;
 
 	@Test
 	public void testSameName() throws IllegalValueException {
-		Name first = new Name("Avery");
-		Name second = new Name("Avery");
+		try {
+			Name first = new Name("Avery");
+			Name second = new Name("Avery");
+		}catch(IllegalValueException ie) {
+			throw new IllegalValueException("test names should be valid");
+		}
+			assertTrue(first.isSimilar(second));
+			assertTrue(second.isSimilar(first));
+	}	
+	
+	
+	@Test
+	public void testDifferentCaseName() throws IllegalValueException {
+		first = new Name("Avery");
+		second = new Name("AVeRy");
 		assertTrue(first.isSimilar(second));
 		assertTrue(second.isSimilar(first));
 	}
